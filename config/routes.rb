ca6661633d2c63d0f0d7320faa3b5b8ad19e566d
yalab <rudeboyjet@gmail.com>
2013-06-05 21:00:16 +0900

    Set root_path.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Optimize::Application.routes.draw do
-  get "root/index"
   resources :users
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'root#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
