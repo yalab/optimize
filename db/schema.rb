4fe16eb3e2882c546ab90e420db21186b4148914
yalab <rudeboyjet@gmail.com>
2013-06-08 13:49:44 +0900

    rails g model Group name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130606054326) do
+ActiveRecord::Schema.define(version: 20130608044922) do
+
+  create_table "groups", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
