1e980583235c5f929b1d399e04065a8589352a20
yalab <rudeboyjet@gmail.com>
2013-06-08 14:26:39 +0900

    Prepare db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,28 @@
-ActiveRecord::Base.transaction do
-  50000.times do
-    User.create(name: Faker::Name.name, age: rand(50))
+MAX_ROWS = 50000
+
+if User.count < MAX_ROWS
+  ActiveRecord::Base.transaction do
+    (MAX_ROWS - User.count).times do
+      User.create(name: Faker::Name.name, age: rand(50))
+    end
   end
+end
+
+if Group.count < MAX_ROWS
+  ActiveRecord::Base.transaction do
+    (MAX_ROWS - Group.count).times do
+      Group.create(name: Faker::Lorem.word)
+    end
+  end
+end
+
+if MemberShip.count < 1
+  max_group_id = Group.maximum(:id)
+  ActiveRecord::Base.transaction do
+    User.all.find_in_batches do |users|
+      users.each do |user|
+        user.member_ships.create(group_id: rand(max_group_id))
+      end
+    end
+  end  
 end
